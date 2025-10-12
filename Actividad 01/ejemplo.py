ecd54f261e92d52074c73b27aeec54852daaaa62
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2025-10-12 12:19:03 +0200

    Create ejemplo.py

---
+++ Actividad 01/ejemplo.py
@@ -0,0 +1,10 @@
+class Persona:
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+
+    def saludar(self):
+        print(f"Hola, soy {self.nombre}")
+
+    def __str__(self):
+        return f"{self.nombre}, {self.edad} años"
