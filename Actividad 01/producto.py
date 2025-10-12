ee8813f32bb36a31feb4d748c717bdf8677f452b
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2025-10-12 22:05:41 +0200

    Update producto.py
    Añadida validación

--- Actividad 01/producto.py
+++ Actividad 01/producto.py
@@ -15,20 +15,47 @@ class Producto:
     def __str__(self):
         return f"ID:{self.id}, Nombre: {self.nombre}, Precio: {self.precio:.2f}, Stock: {self.cantidad}"
 
+# -------------------------------------------------------
 # Función para crear producto con input del usuario
+# -------------------------------------------------------
 
 
 def crear_producto(productos):
+    # Solicita el nombre del producto
     nombre = input("Introduce nombre del producto: ")
-    precio = float(
-        input("Introduce precio del producto:(ejemplo 199.99) "))
-    cantidad = int(input("Introduce la cantidad de productos en stock: "))
+    # Validación para nombre
+    if not nombre:
+        print("El nombre no puede estar vacío.")
+        return
 
-    # Crear producto y añadir a la lista
+    try:
+        # Solicita y valida el precio como float
+        precio = float(
+            input("Introduce precio del producto:(ejemplo 199.99) "))
+        if precio <= 0:
+            print("El precio debe ser un número positivo")
+            return
+    except ValueError:
+        # Captura error si el formato del precio es incorrecto
+        print("Formato de precio inválido.")
+        return
+
+    try:
+        # Solicita y valida la cantidad como int
+        cantidad = int(input("Introduce la cantidad de productos en stock: "))
+        if cantidad < 0:
+            print("La cantidad debe ser un número positivo")
+            return
+    except ValueError:
+        # Captura error si el formato de la cantidad es incorrecto
+        print("Formato de cantidad en inventario inválido.")
+        return
+
+    # Crea el nuevo objeto Producto
     nuevo_producto = Producto(nombre, precio, cantidad)
+    # Agrega el nuevo producto a la lista
     productos.append(nuevo_producto)
 
-    # Confirmar inclusión en la lista e imprimir producto nuevo
     print(f"Producto nuevo añadido al inventario.")
     print(nuevo_producto)
 
@@ -37,11 +64,21 @@ def listar_productos(productos):
     print("Sigue el listado de los productos de la tienda: ")
 
     for producto in productos:
+        # Imprime cada producto
         print(producto)
+
+# -------------------------------------------------------
+# Función para actualizar producto con input del usuario
+# -------------------------------------------------------
 
 
 def actualizar_productos(productos):
-    id_actualizar = int(input("Introduce el ID a actualizar: "))
+    try:
+        # Solicita el ID a actualizar y valida la entrada
+        id_actualizar = int(input("Introduce el ID a actualizar: "))
+    except ValueError:
+        print("ID inválido.")
+        return
 
     producto_encontrado_actualizado = False
 
@@ -55,25 +92,46 @@ def actualizar_productos(productos):
                 producto.nombre = nuevo_nombre
 
             # Cambiar cantidad en inventario
-            nueva_cantidad = int(
-                input(f"Nueva cantidad: (dejalo en blanco para no cambiar nada)"))
+            nueva_cantidad = input(
+                f"Nueva cantidad: (dejalo en blanco para no cambiar nada)")
             if nueva_cantidad:
-                producto.cantidad = nueva_cantidad
-                producto_encontrado_actualizado = True
+                try:
+                    nueva_cantidad = int(nueva_cantidad)
+
+                    if nueva_cantidad >= 0:
+                        producto.cantidad = nueva_cantidad
+                        producto_encontrado_actualizado = True
+                    else:
+                        print("La cantidad deve ser un número positivo o cero.")
+                except ValueError:
+                    print("La cantidad debe ser un número.")
 
             # Cambiar precio del producto
-            nuevo_precio = float(
-                input(f"Nuevo precio: (dejalo en blanco para no cambiar nada)"))
+            nuevo_precio = input(
+                f"Nuevo precio: (dejalo en blanco para no cambiar nada)")
             if nuevo_precio:
-                producto.precio = nuevo_precio
-                producto_encontrado_actualizado = True
+                try:
+                    nuevo_precio = float(nuevo_precio)
+
+                    if nuevo_precio >= 0:
+                        producto.precio = nuevo_precio
+                        producto_encontrado_actualizado = True
+                    else:
+                        print("El precio debe ser un número positivo o cero.")
+                except ValueError:
+                    print("El precio debe ser un número.")
 
     if not producto_encontrado_actualizado:
-        print("Producto no encontrado")
+        print("Producto no encontrado o no actualizado.")
+
+# -------------------------------------------------------
+# Función para eliminar producto con input del usuario
+# -------------------------------------------------------
 
 
 def eliminar_producto(productos):
     try:
+        # Solicita el ID a eliminar y valida la entrada
         id_eliminar = int(input("Introduce el ID del producto a eliminar: "))
     except ValueError:
         print("ID inválido.")
