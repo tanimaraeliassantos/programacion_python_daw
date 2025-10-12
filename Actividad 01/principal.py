eb8f686a14e9aee93ee224f6375a7bb9a13c9b97
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2025-10-12 15:20:48 +0200

    Update principal.py
    Creadas opciones crear productos y listar productos en menu principal

--- Actividad 01/principal.py
+++ Actividad 01/principal.py
@@ -1,11 +1,39 @@
-from producto import Producto
-
+from producto import Producto, crear_producto, listar_productos
+# List vacía para almacenar productos
 productos = []
 
-p1 = Producto("Portatil Acer Gaming Nitro V 15\"", 649, 00, 8)
-p2 = Producto("Portatil Acer Aspire Go 15 AG15", 479, 00, 10)
-p3 = Producto("Portatil Lenovo IdeaPad Slim 3", 349, 00, 20)
+# Productos para iniciar lista
+p1 = Producto("Portatil Acer Gaming Nitro V 15\"", 649.00, 8)
+p2 = Producto("Portatil Acer Aspire Go 15 AG15", 479.00, 10)
+p3 = Producto("Portatil Lenovo IdeaPad Slim 3", 349.00, 20)
 
 productos.append(p1)
 productos.append(p2)
 productos.append(p3)
+
+# Inicializar función para disponer de menu principal
+def menu_principal(productos):
+    ejecutando = True
+
+    while ejecutando:
+        print("*** Menu de gestión de productos *** ")
+        print("1. Crear nuevo producto")
+        print("2. Listar todos los productos")
+
+        opcion = input("Elige que deseas hacer: (1-5)")
+
+        if opcion == "1":
+            crear_producto(productos)
+
+        elif opcion == "2":
+            listar_productos(productos)
+
+        elif opcion == "5":
+            print("Gestor de productos finalizado.")
+            ejecutando = False
+        else:
+            print("Opcion no válida, inténtalo de nuevo.")
+
+
+if __name__ == "__main__":
+    menu_principal(productos)
