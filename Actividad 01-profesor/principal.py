2440fa491ca8db4d77f330f74936ecfac589df8c
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2025-10-12 14:43:30 +0200

    Create principal.py

---
+++ Actividad 01-profesor/principal.py
@@ -0,0 +1,38 @@
+from ejemplo import Persona
+
+if __name__ == "__main__":
+    print("Hola!")
+    nombre = input("Nombre: ")
+    edad = input("Edad: ")
+
+    nueva = Persona(nombre, edad)
+    print(Persona.id)
+    nueva2 = Persona(nombre, edad)
+    print(nueva2.nombre)
+
+    """
+    Para 
+    comentarios
+    en
+    bloque
+    
+    """
+    nombres = ["Ana", "Maria", "Paula"]
+    for nombre in nombres:
+        print("Hola", nombre)
+
+    p1 = Persona("Ana", 25)
+    p2 = Persona("Luis", 30)
+    p3 = Persona("Marta", 45)
+
+    personas = []
+    personas.append(p1)
+    personas.append(p2)
+    personas.append(p3)
+
+    for per in personas:
+        per.saludar()
+
+    edad = p1.edad
+    if edad == 25:
+        print(f"{p1.nombre} tiene {edad} años")
