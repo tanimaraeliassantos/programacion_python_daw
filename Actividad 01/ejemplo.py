9b0d6b17e40cb81a7295cc903865d026970836f9
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2025-10-12 12:55:53 +0200

    Update ejemplo.py

--- Actividad 01/ejemplo.py
+++ Actividad 01/ejemplo.py
@@ -1,7 +1,11 @@
 class Persona:
+    id = 1
+
     def __init__(self, nombre, edad):
         self.nombre = nombre
         self.edad = edad
+        self.id = Persona.id
+        Persona.id = Persona.id + 1
 
     def saludar(self):
         print(f"Hola, soy {self.nombre}")
