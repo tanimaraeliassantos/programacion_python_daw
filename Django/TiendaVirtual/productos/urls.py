5b2078aa880164a7a39057025676d6139e6911de
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2025-11-11 21:56:42 +0100

    Cambios para que CRUD funcione

--- Django/TiendaVirtual/productos/urls.py
+++ Django/TiendaVirtual/productos/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.lista_productos, name='lista_productos'),
-    path('/', views.detalle_producto, name='detalle_producto'),
+    path('<int:producto_id>/', views.detalle_producto, name='detalle_producto'),
     path('nuevo/', views.crear_producto, name='crear_producto'),
-    path('/editar/', views.editar_producto, name='editar_producto'),
-    path('/eliminar/', views.eliminar_producto, name='eliminar_producto'),
+    path('<int:producto_id>/editar/',
+         views.editar_producto, name='editar_producto'),
+    path('<int:producto_id>/eliminar/',
+         views.eliminar_producto, name='eliminar_producto'),
 ]
--- Django/TiendaVirtual/productos/views.py
+++ Django/TiendaVirtual/productos/views.py
@@ -1,3 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Producto
 from .forms import ProductoForm
@@ -12,3 +15,28 @@ def lista_productos(request):
 def detalle_producto(request, producto_id):
     producto = get_object_or_404(Producto, id=producto_id)
     return render(request, 'productos/detalle_producto.html', {'producto': producto})
+
+
+def crear_producto(request):
+    form = ProductoForm(request.POST or None)
+    if request.method == 'POST' and form.is_valid():
+        p = form.save()
+        return redirect('detalle_producto', producto_id=p.id)
+    return render(request, 'productos/crear_producto.html', {'form': form})
+
+
+def editar_producto(request, producto_id):
+    producto = get_object_or_404(Producto, id=producto_id)
+    form = ProductoForm(request.POST or None, instance=producto)
+    if request.method == 'POST' and form.is_valid():
+        form.save()
+        return redirect('detalle_producto', producto_id=producto.id)
+    return render(request, 'productos/editar_producto.html', {'form': form, 'producto': producto})
+
+
+def eliminar_producto(request, producto_id):
+    producto = get_object_or_404(Producto, id=producto_id)
+    if request.method == 'POST':
+        producto.delete()
+        return redirect('lista_productos')
+    return render(request, 'productos/eliminar_producto.html', {'producto': producto})
