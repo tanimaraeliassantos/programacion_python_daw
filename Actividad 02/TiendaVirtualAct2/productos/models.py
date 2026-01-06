765c11492f245c7e7bcea68d217eab2cfeeafba2
Tanimara Elias Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2026-01-06 17:18:24 +0100

    Update models.py
    Removida Categoria
    Corregido modelo

--- Actividad 02/TiendaVirtualAct2/productos/models.py
+++ Actividad 02/TiendaVirtualAct2/productos/models.py
@@ -1,17 +1,9 @@
 from django.db import models
 
 
-class Categoria(models.Model):
-    nombre = models.CharField(max_length=100)
-    descripcion = models.TextField(blank=True)
-
-    def __str__(self):
-        return self.nombre
-
-
 class Producto(models.Model):
     nombre = models.CharField(max_length=200)
-    precio = models.DecimalField(max_digits=10, decimal_places=2),
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
     cantidad = models.IntegerField(default=0)
 
     def __str__(self):
